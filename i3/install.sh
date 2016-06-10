1de2a2abd74470f9b682fdc4e59ac980c3d7211f
icyheights <icyheights@yandex.ru>
2016-06-10 12:08:25 +0300

    Fix install.sh

--- i3/install.sh
+++ i3/install.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-I3DIR="~/.i3"
-I3CONFIG="$I3DIR/config"
-I3STATUSCONFIG="~/.i3status.conf"
-AUTORUN="$I3DIR/autorun.sh"
+I3DIR=~/.i3
+I3CONFIG=$I3DIR/config
+I3STATUSCONFIG=~/.i3status.conf
+AUTORUN=$I3DIR/autorun.sh
 
-mkdir $I3DIR
-cp ./config $I3CONFIG
-cp ./i3status.conf $I3STATUSCONFIG
-cp ./autorun.sh $AUTORUN
+mkdir -p $I3DIR
+cp -i ./config $I3CONFIG
+cp -i ./i3status.conf $I3STATUSCONFIG
+cp -i ./autorun.sh $AUTORUN
 chmod 744 $AUTORUN
